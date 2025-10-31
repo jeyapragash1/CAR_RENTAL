46625fee87b22373efeec353af98d8b43db6b93c
jeyapragash <kishojeyapragash15@gmail.com>
2025-10-31 11:53:48 +0530

    first

---
+++ backend/app/Models/Rental.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Rental extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'customer_id',
+        'vehicle_id',
+        'rent_start_date',
+        'rent_end_date',
+        'total_amount',
+        'status', // pending / approved / rejected
+    ];
+
+    // Relationships
+    public function customer()
+    {
+        return $this->belongsTo(Customer::class);
+    }
+
+    public function vehicle()
+    {
+        return $this->belongsTo(Vehicle::class);
+    }
+}
